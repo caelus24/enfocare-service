73c4c40c13386f4e1399945e28a4a80564c43f64
Spheenks <salimbaojoshua@gmail.com>
2025-02-03 02:57:51 +0800

    new update

--- src/main/java/com/enfocareservice/config/WebSocketConfig.java
+++ src/main/java/com/enfocareservice/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/enfocare/chat/ws")
-                .setAllowedOrigins("https://enfocare-service-production.up.railway.app") // Explicitly allow Railway
+                .setAllowedOrigins("https://enfocare-service-production.up.railway.app", "*") // Allow all origins for testing
                 .withSockJS(); // Ensure compatibility with non-WebSocket clients
     }
 
