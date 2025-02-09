370ae907d466c8d09882f92468ff9afd51c19e72
Chris <chris.manlangit@gmail.com>
2025-02-09 09:34:36 +0800

    new update

--- src/main/java/com/enfocareservice/service/ChatService.java
+++ src/main/java/com/enfocareservice/service/ChatService.java
@@ -29,107 +29,123 @@ import com.enfocareservice.repository.UserRepository;
 @Service
 public class ChatService {
 
-	private static final Logger logger = LoggerFactory.getLogger(ChatService.class);
+    private static final Logger logger = LoggerFactory.getLogger(ChatService.class);
 
-	@Autowired
-	private MessageRepository messageRepository;
+    @Autowired
+    private MessageRepository messageRepository;
 
-	@Autowired
-	private ChatSessionRepository chatSessionRepository;
+    @Autowired
+    private ChatSessionRepository chatSessionRepository;
 
-	@Autowired
-	private SessionMessageRepository sessionMessageRepository;
+    @Autowired
+    private SessionMessageRepository sessionMessageRepository;
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private ChatAppMapper chatAppMapper;
+    @Autowired
+    private ChatAppMapper chatAppMapper;
 
-	@Transactional
-	public SessionMessageEntity sendMessage(Integer sessionId, Integer senderId, Integer receiverId,
-			String messageText) {
+    @Transactional
+    public SessionMessageEntity sendMessage(Integer sessionId, Integer senderId, Integer receiverId,
+                                            String messageText) {
+        logger.info("sendMessage called with sessionId: {}, senderId: {}, receiverId: {} TUNAMAYO", sessionId, senderId, receiverId);
+        try {
+            UserEntity sender = userRepository.findById(senderId)
+                    .orElseThrow(() -> new IllegalArgumentException("Sender not found with ID: " + senderId));
+            UserEntity receiver = userRepository.findById(receiverId)
+                    .orElseThrow(() -> new IllegalArgumentException("Receiver not found with ID: " + receiverId));
+            ChatSessionEntity chatSession = chatSessionRepository.findById(sessionId)
+                    .orElseThrow(() -> new IllegalArgumentException("Chat session does not exist with ID: " + sessionId));
 
-		System.err.println("ChatService - sendMessage " + "SESSION ID " + sessionId + "MESSAGE TEXT " + messageText
-				+ " senderId " + senderId);
+            MessageEntity messageEntity = new MessageEntity();
+            messageEntity.setSender(sender);
+            messageEntity.setReceiver(receiver);
+            messageEntity.setMessageText(messageText);
+            messageEntity.setTimestamp(Timestamp.valueOf(LocalDateTime.now()));
+            messageEntity.setSession(chatSession);
+            messageEntity = messageRepository.save(messageEntity);
 
-		UserEntity sender = userRepository.findById(senderId)
-				.orElseThrow(() -> new IllegalArgumentException("Sender not found with ID: " + senderId));
-		UserEntity receiver = userRepository.findById(receiverId)
-				.orElseThrow(() -> new IllegalArgumentException("Receiver not found with ID: " + receiverId));
-		ChatSessionEntity chatSession = chatSessionRepository.findById(sessionId)
-				.orElseThrow(() -> new IllegalArgumentException("Chat session does not exist with ID: " + sessionId));
+            SessionMessageEntity sessionMessageEntity = new SessionMessageEntity();
+            sessionMessageEntity.setChatSession(chatSession);
+            sessionMessageEntity.setMessage(messageEntity);
 
-		MessageEntity messageEntity = new MessageEntity();
-		messageEntity.setSender(sender);
-		messageEntity.setReceiver(receiver);
-		messageEntity.setMessageText(messageText);
-		messageEntity.setTimestamp(Timestamp.valueOf(LocalDateTime.now()));
-		messageEntity.setSession(chatSession);
-		messageEntity = messageRepository.save(messageEntity);
+            long sessIdLong = sessionId;
+            sessionMessageEntity.setId(new SessionMessageId(sessIdLong, messageEntity.getId()));
 
-		SessionMessageEntity sessionMessageEntity = new SessionMessageEntity();
-		sessionMessageEntity.setChatSession(chatSession);
-		sessionMessageEntity.setMessage(messageEntity);
+            SessionMessageEntity sessionMessageEntityRes = sessionMessageRepository.save(sessionMessageEntity);
+            logger.info("Message sent and recorded with messageId {} TUNAMAYO", messageEntity.getId());
+            return sessionMessageEntityRes;
+        } catch (Exception e) {
+            logger.error("Error sending message TUNAMAYO", e);
+            throw e;
+        }
+    }
 
-		long sessIdLong = sessionId;
-		sessionMessageEntity.setId(new SessionMessageId(sessIdLong, messageEntity.getId()));
+    public List<Message> getMessagesForSessionAndParticipant(Integer sessionId, Integer participantId) {
+        logger.debug("Fetching messages for session {} and participant {} TUNAMAYO", sessionId, participantId);
+        try {
+            ChatSessionEntity session = chatSessionRepository.findById(sessionId)
+                    .orElseThrow(() -> new IllegalArgumentException("Session not found"));
+            UserEntity participant = userRepository.findById(participantId)
+                    .orElseThrow(() -> new IllegalArgumentException("Participant not found"));
 
-		SessionMessageEntity sessionMessageEntityRes = sessionMessageRepository.save(sessionMessageEntity);
-		logger.info("Message sent and recorded with messageId {}", messageEntity.getId());
-		return sessionMessageEntityRes;
-	}
+            List<MessageEntity> messageEntities = messageRepository.findMessagesBySessionAndParticipant(session, participant);
+            return messageEntities.stream().map(chatAppMapper::mapToMessageModel).collect(Collectors.toList());
+        } catch (Exception e) {
+            logger.error("Error fetching messages TUNAMAYO", e);
+            throw e;
+        }
+    }
 
-	public List<Message> getMessagesForSessionAndParticipant(Integer sessionId, Integer participantId) {
-		logger.debug("Fetching messages for session {} and participant {}", sessionId, participantId);
-		ChatSessionEntity session = chatSessionRepository.findById(sessionId)
-				.orElseThrow(() -> new IllegalArgumentException("Session not found"));
-		UserEntity participant = userRepository.findById(participantId)
-				.orElseThrow(() -> new IllegalArgumentException("Participant not found"));
+    @Transactional
+    public ChatSessionEntity createSession(String sessionName, Set<Integer> participantIds) {
+        logger.debug("Creating chat session with name {}", sessionName);
+        try {
+            ChatSessionEntity chatSession = new ChatSessionEntity();
+            chatSession.setSessionName(sessionName);
+            chatSession.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
 
-		List<MessageEntity> messageEntities = messageRepository.findMessagesBySessionAndParticipant(session,
-				participant);
-		return messageEntities.stream().map(chatAppMapper::mapToMessageModel).collect(Collectors.toList());
-	}
+            Set<SessionParticipantEntity> participants = chatSession.getParticipants();
+            participants.clear();
 
-	@Transactional
-	public ChatSessionEntity createSession(String sessionName, Set<Integer> participantIds) {
-		logger.debug("Creating chat session with name {}", sessionName);
-		ChatSessionEntity chatSession = new ChatSessionEntity();
-		chatSession.setSessionName(sessionName);
-		chatSession.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
+            participantIds.forEach(participantId -> {
+                UserEntity participant = userRepository.findById(participantId)
+                        .orElseThrow(() -> new IllegalArgumentException("User not found with ID: " + participantId));
+                SessionParticipantEntity sessionParticipant = new SessionParticipantEntity();
+                sessionParticipant.setChatSession(chatSession);
+                sessionParticipant.setUser(participant);
+                sessionParticipant.setJoinedAt(Timestamp.valueOf(LocalDateTime.now()));
 
-		Set<SessionParticipantEntity> participants = chatSession.getParticipants();
-		participants.clear();
+                SessionParticipantId participantIdObj = new SessionParticipantId();
+                participantIdObj.setUserId(participant.getId());
+                sessionParticipant.setId(participantIdObj);
 
-		participantIds.forEach(participantId -> {
-			UserEntity participant = userRepository.findById(participantId)
-					.orElseThrow(() -> new IllegalArgumentException("User not found with ID: " + participantId));
-			SessionParticipantEntity sessionParticipant = new SessionParticipantEntity();
-			sessionParticipant.setChatSession(chatSession);
-			sessionParticipant.setUser(participant);
-			sessionParticipant.setJoinedAt(Timestamp.valueOf(LocalDateTime.now()));
+                participants.add(sessionParticipant);
+            });
 
-			SessionParticipantId participantIdObj = new SessionParticipantId();
-			participantIdObj.setUserId(participant.getId());
-			sessionParticipant.setId(participantIdObj);
+            ChatSessionEntity savedSession = chatSessionRepository.save(chatSession);
+            logger.info("Chat session created with ID {}TUNAMAYO", savedSession.getSessionId());
+            return savedSession;
+        } catch (Exception e) {
+            logger.error("Error creating chat session TUNAMAYO", e);
+            throw e;
+        }
+    }
 
-			participants.add(sessionParticipant);
-		});
-
-		ChatSessionEntity savedSession = chatSessionRepository.save(chatSession);
-		logger.info("Chat session created with ID {}", savedSession.getSessionId());
-		return savedSession;
-	}
-
-	public ChatSessionEntity fetchLatestSessionByParticipants(Set<Integer> participantIds) {
-		logger.debug("Fetching latest session for participants {}", participantIds);
-		List<ChatSessionEntity> sessions = chatSessionRepository.findLatestSessionByParticipants(participantIds,
-				participantIds.size());
-		if (sessions.isEmpty()) {
-			logger.warn("No sessions found for given participants");
-			throw new IllegalStateException("No session available for the provided participant IDs");
-		}
-		return sessions.get(0);
-	}
+    public ChatSessionEntity fetchLatestSessionByParticipants(Set<Integer> participantIds) {
+        logger.debug("Fetching latest session for participants {}", participantIds);
+        try {
+            List<ChatSessionEntity> sessions = chatSessionRepository.findLatestSessionByParticipants(participantIds,
+                    participantIds.size());
+            if (sessions.isEmpty()) {
+                logger.warn("No sessions found for given participants");
+                throw new IllegalStateException("No session available for the provided participant IDs");
+            }
+            return sessions.get(0);
+        } catch (Exception e) {
+            logger.error("Error fetching latest session TUNAMAYO", e);
+            throw e;
+        }
+    }
 }
--- src/main/java/com/enfocareservice/service/MedicalFileService.java
+++ src/main/java/com/enfocareservice/service/MedicalFileService.java
@@ -15,6 +15,8 @@ import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
 import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -29,125 +31,109 @@ import com.enfocareservice.repository.MedicalFileRepository;
 
 @Service
 public class MedicalFileService {
+    
+    private static final Logger logger = LoggerFactory.getLogger(MedicalFileService.class);
 
-	@Autowired
-	private MedicalFileRepository medicalFileRepository;
+    @Autowired
+    private MedicalFileRepository medicalFileRepository;
 
-	@Autowired
-	private MedicalFileMapper medicalFileMapper;
+    @Autowired
+    private MedicalFileMapper medicalFileMapper;
 
-	@Value("${medicalfile.dir}")
-	private String diagnosisDir;
+    @Value("${medicalfile.dir}")
+    private String diagnosisDir;
 
-	public List<String> getRecipentEmailsList(String doctorEmail) {
+    public List<String> getRecipentEmailsList(String doctorEmail) {
+        return medicalFileRepository.findDistinctPatientEmailsByDoctorEmail(doctorEmail);
+    }
 
-		return medicalFileRepository.findDistinctPatientEmailsByDoctorEmail(doctorEmail);
-	}
+    public void protectPdfFile(String paramFile) throws MalformedURLException {
+        File file = new File(paramFile);
+        logger.info("Processing file encryption: {}", file.getAbsolutePath());
 
-	public void protectPdfFile(String paramFile) throws MalformedURLException {
-		File file = new File(paramFile);
-		System.err.println("FILENAME RAW : " + file.getAbsolutePath());
-		System.err.println("FILENAMEs : " + paramFile);
+        try {
+            PDDocument document = Loader.loadPDF(file);
+            AccessPermission ap = new AccessPermission();
+            StandardProtectionPolicy spp = new StandardProtectionPolicy("123456", "123456", ap);
+            spp.setEncryptionKeyLength(128);
+            spp.setPermissions(ap);
+            document.protect(spp);
+            logger.info("Document encrypted successfully.");
 
-		try {
-			PDDocument document = Loader.loadPDF(file);
+            File tempFile = File.createTempFile("temp_", ".pdf");
+            document.save(tempFile.getAbsolutePath());
+            document.close();
+            Files.move(tempFile.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            logger.error("Error encrypting PDF file: {}", paramFile, e);
+        }
+    }
 
-			AccessPermission ap = new AccessPermission();
+    public void uploadDiagnosisFile(String patientEmail, String doctorEmail, MultipartFile file, Long consultationId) {
+        try {
+            logger.info("Uploading diagnosis file for patient: {} by doctor: {}", patientEmail, doctorEmail);
+            String modifiedEmail = patientEmail.replaceAll("@|\\.", "");
+            String directoryPath = diagnosisDir + File.separator + modifiedEmail;
+            File directory = new File(directoryPath);
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
 
-			StandardProtectionPolicy spp = new StandardProtectionPolicy("123456", "123456", ap);
+            String originalFilename = file.getOriginalFilename();
+            String filePath = Paths.get(directoryPath, originalFilename).toString();
 
-			spp.setEncryptionKeyLength(128);
+            Files.copy(file.getInputStream(), Path.of(filePath));
+            String password = generatePassword();
 
-			// Setting the access permissions
-			spp.setPermissions(ap);
+            MedicalFileEntity medicalFileEntity = new MedicalFileEntity();
+            medicalFileEntity.setPatientEmail(patientEmail);
+            medicalFileEntity.setDoctorEmail(doctorEmail);
+            medicalFileEntity.setFilePath(filePath);
+            medicalFileEntity.setPassword(password);
+            medicalFileEntity.setConsultationId(consultationId);
 
-			// Protecting the document
-			document.protect(spp);
+            medicalFileRepository.save(medicalFileEntity);
+            logger.info("File uploaded successfully: {}", filePath);
+        } catch (IOException e) {
+            logger.error("Error uploading file for patient: {}", patientEmail, e);
+        }
+    }
 
-			System.out.println("Document encrypted");
+    private String generatePassword() {
+        return "generatedPassword";
+    }
 
-			// Create a temporary file to save the encrypted PDF
-			File tempFile = File.createTempFile("temp_", ".pdf");
+    public List<String> getFilePathsForPatient(String patientEmail) {
+        return medicalFileRepository.findFilePathsByPatientEmail(patientEmail);
+    }
 
-			// Saving the document to the temporary file
-			document.save(tempFile.getAbsolutePath());
+    public Resource loadFileAsResource(Long fileId) throws MalformedURLException {
+        try {
+            Optional<MedicalFileEntity> fileEntityOptional = medicalFileRepository.findById(fileId);
+            if (fileEntityOptional.isPresent()) {
+                protectPdfFile(fileEntityOptional.get().getFilePath());
+                Path filePath = Paths.get(fileEntityOptional.get().getFilePath()).normalize();
+                Resource resource = new UrlResource(filePath.toUri());
+                if (resource.exists()) {
+                    return resource;
+                } else {
+                    logger.error("File not found: {}", fileId);
+                    throw new MalformedURLException("File not found " + fileId);
+                }
+            } else {
+                logger.error("File entity not found for ID: {}", fileId);
+                throw new MalformedURLException("File entity not found " + fileId);
+            }
+        } catch (Exception e) {
+            logger.error("Error loading file as resource for ID: {}", fileId, e);
+            throw new MalformedURLException("Error loading file " + fileId);
+        }
+    }
 
-			// Closing the document
-			document.close();
-
-			// Rename the temporary file to replace the original file
-			Files.move(tempFile.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	public void uploadDiagnosisFile(String patientEmail, String doctorEmail, MultipartFile file, Long consultationId)
-			throws IOException {
-
-		System.err.println("FILENAME : " + file.getName());
-		String modifiedEmail = patientEmail.replaceAll("@|\\.", "");
-		String directoryPath = diagnosisDir + File.separator + modifiedEmail;
-		File directory = new File(directoryPath);
-		if (!directory.exists()) {
-			directory.mkdirs();
-		}
-
-		String originalFilename = file.getOriginalFilename();
-		String filePath = Paths.get(directoryPath, originalFilename).toString();
-
-		// Save the file locally
-		Files.copy(file.getInputStream(), Path.of(filePath));
-
-		// Generate a password for the file, assuming generatePassword() is implemented
-		// elsewhere
-		String password = generatePassword();
-
-		// Create and save the MedicalFileEntity
-		MedicalFileEntity medicalFileEntity = new MedicalFileEntity();
-		medicalFileEntity.setPatientEmail(patientEmail);
-		medicalFileEntity.setDoctorEmail(doctorEmail);
-		medicalFileEntity.setFilePath(filePath);
-		medicalFileEntity.setPassword(password);
-		medicalFileEntity.setConsultationId(consultationId);
-
-		medicalFileRepository.save(medicalFileEntity);
-	}
-
-	// Placeholder for the generatePassword method
-	private String generatePassword() {
-		// Implement password generation logic here
-		return "generatedPassword";
-	}
-
-	public List<String> getFilePathsForPatient(String patientEmail) {
-		return medicalFileRepository.findFilePathsByPatientEmail(patientEmail);
-	}
-
-	public Resource loadFileAsResource(Long fileId) throws MalformedURLException {
-		Optional<MedicalFileEntity> fileEntityOptional = medicalFileRepository.findById(fileId);
-		if (fileEntityOptional.isPresent()) {
-
-			protectPdfFile(fileEntityOptional.get().getFilePath());
-			Path filePath = Paths.get(fileEntityOptional.get().getFilePath()).normalize();
-			Resource resource = new UrlResource(filePath.toUri());
-			if (resource.exists()) {
-				return resource;
-			} else {
-				// Handle the case where the file doesn't exist or is not accessible
-				throw new MalformedURLException("File not found " + fileId);
-			}
-		} else {
-			// Handle the case where the file entity was not found in the database
-			throw new MalformedURLException("File entity not found " + fileId);
-		}
-	}
-
-	public List<MedicalFile> getFilesByConsultationId(Long consultationId) {
-		// TODO Auto-generated method stub
-		return medicalFileRepository.findByConsultationId(consultationId).stream()
-				.map(medicalFileEntity -> medicalFileMapper.map(medicalFileEntity)).collect(Collectors.toList());
-	}
-
+    public List<MedicalFile> getFilesByConsultationId(Long consultationId) {
+        logger.info("Fetching files for consultation ID: {}", consultationId);
+        return medicalFileRepository.findByConsultationId(consultationId).stream()
+                .map(medicalFileEntity -> medicalFileMapper.map(medicalFileEntity))
+                .collect(Collectors.toList());
+    }
 }
